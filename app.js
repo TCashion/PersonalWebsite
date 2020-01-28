54d23ee5ee26bfaaff72e1c1147ce1074e4ec2e1
TCashion <58757351+TCashion@users.noreply.github.com>
2020-01-28 06:52:16 -0700

    Automated header and footer using jQuery template string literal
    Header and footer are now consistent on all pages, just need to be edited in app.js

--- app.js
+++ app.js
@@ -2,4 +2,77 @@ $(document).ready(function() {
 
     console.log("SANITY CHECK");
 
+
+    // ************************************
+    // *********** HEADER *****************
+
+    // Inserts header template on every page. Allows for easy editing of header on all pages.
+    // Edit header here: 
+    var headerTemplate = `
+        <header>        
+            <div class="container">
+                <div class="row">
+                    <div class="col col-12" >
+                        <div class="row">
+                            <div id="nameBanner" class="col col-12">
+                                <h1>Travis G. Cashion</h1>
+                            </div>
+                            <div>
+                                <img id="srcbutton" src="images/SearchIcon.PNG">
+                            </div>
+                        </div>
+                        <nav>
+                            <div class="row" id="nav-links">
+                                <div class="col">
+                                    <a href="../PersonalWebsite/index.html">Home</a>
+                                </div>
+                                <div class="col">
+                                    <a href="../PersonalWebsite/about.html">About</a>
+                                </div>
+                                <div class="col">
+                                    <a href="../PersonalWebsite/blog.html">Blog</a>
+                                </div>
+                                <div class="col">
+                                    <a href="../PersonalWebsite/gallery.html">Gallery</a>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
+    `;
+
+    $("#headerDiv").append(headerTemplate);
+    // *********** END HEADER *************
+    // ************************************
+
+    
+    // ************************************
+    // *********** FOOTER *****************
+    var footerTemplate = `
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col col-4">
+                        <h1>Travis G. Cashion</h1>
+                    </div>
+                    <div class="col col-4">
+                        &copy; 2020
+                        <p>Web design & formatting by Travis Cashion</p>
+                    </div>
+                    <div class="col col-4 social" >
+                        <img class="socialimgs" src="images/email.png">
+                        <img class="socialimgs" src="images/instagram.png">
+                        <img class="socialimgs" src="images/medium.png">
+                        <img class="socialimgs" src="images/facebook.png">
+                    </div>
+                </div>
+            </div>
+        </footer>
+    `;
+    
+    $("#footerDiv").append(footerTemplate);
+    // *********** END FOOTER *************
+    // ************************************
 });
