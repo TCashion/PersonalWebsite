3aa90a78865d779df923a5a5ebb8431c2dfec6a3
TCashion <58757351+TCashion@users.noreply.github.com>
2020-02-21 19:06:41 -0700

    created nested class for blog pages so header updates with correct file pathways

--- app.js
+++ app.js
@@ -47,7 +47,47 @@ $(document).ready(function() {
         </header>
     `;
 
+    var headerTemplateNested = `
+        <header>        
+            <div class="container">
+                <div class="row">
+                    <div class="col col-12" >
+                        <div class="row">
+                            <div id="nameBanner" class="col col-12">
+                                <h1>Travis G. Cashion</h1>
+                            </div>
+                            <div>
+                                <img id="srcbutton" src="../images/SearchIcon.PNG">
+                            </div>
+                        </div>
+                        <nav>
+                            <div class="row" id="nav-links">
+                                <div class="col">
+                                    <a href="../../PersonalWebsite/index.html">Home</a>
+                                </div>
+                                <div class="col">
+                                    <a href="../../PersonalWebsite/portfolio.html">Portfolio</a>
+                                </div>
+                                <div class="col">
+                                    <a href="../../PersonalWebsite/blog.html">Blog</a>
+                                </div>
+                                <!-- COMMENTED OUT UNTIL READY
+                                 <div class="col">
+                                    <a href="../../PersonalWebsite/gallery.html">Gallery</a>
+                                </div> -->
+                                <div class="col">
+                                    <a href="../../PersonalWebsite/about.html">About</a>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
+    `;
+
     $("#headerDiv").append(headerTemplate);
+    $("#headerDiv-nested").append(headerTemplateNested);
     // *********** END HEADER *************
     // ************************************
 
