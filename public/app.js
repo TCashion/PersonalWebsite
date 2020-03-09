ab210ec25730d4eec457993864fd73d3f5d51871
TCashion <58757351+TCashion@users.noreply.github.com>
2020-03-09 07:11:23 -0600

    progress with photo filter

--- public/app.js
+++ public/app.js
@@ -234,17 +234,43 @@ $(document).ready(function() {
             }; 
         });
 
+        // alternates photos of only filtered elements
+        function alternateFilteredPhoto() {
+            $(".blog-preview").each( function () {
+                if ( $(this).hasClass("alternate-photo") ) {
+                    let child = $(this).children().children(".col-4");
+                    child.first().removeClass("hidden");
+                    child.last().addClass("hidden");
+                }
+            })
+        };
+
+
         // updates the filter classes based on selections 
         if (filterSelected.length === 0) {
             $(".blog-preview").each( function () {
                     $(this).removeClass("hidden");
+                    // RESET:
+                    // remove "atlernate-photo" class from all divs
+                    $(this).removeClass("alternate-photo");
+                    // remove hidden class from all col-4's (photos)
+                    let child = $(this).children().children(".col-4")
+                    child.removeClass("hidden"); 
+                    // call alternate photo function
+                    alternatePreviewPhoto();
             });
         } else {
             $(".blog-preview").addClass("hidden");
+                // Add hidden class to all col-4 photos
             for ( i = 0; i < filterSelected.length; i++) {
                 $(".blog-preview").each( function () { 
                     if ( $(this).hasClass(`${filterSelected[i]}`) ) {
                         $(this).removeClass("hidden");
+                        // for non-hidden divs, alternate photos 
+                        // add another class "alternate-photo"
+                        $(this).addClass("alternate-photo");
+                        // call function that alternates photos based on new class
+                        alternateFilteredPhoto();
                     }; 
                 });
             };
