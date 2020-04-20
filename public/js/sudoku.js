d3695a42750e03515ae1622b88785a2cee8f8da8
TCashion <58757351+TCashion@users.noreply.github.com>
2020-04-19 19:54:46 -0600

    rough draft of sudoku psuedocode

---
+++ public/js/sudoku.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+});
+
+// PROGRAM Sudoku
+
+    // MODULE create board
+        // CREATE 3x3 matrix of numbers 1 - 9
+            // SET as top left box
+        // CREATE adjacent boxes to the right
+            // VERIFY all rows and boxes contain 1-9
+        // CREATE 2ND row of boxes 
+        // CREATE 3rd row of boxes 
+    // END Module
+
+    // MODULE set difficulty
+        // TRACK user selection
+        // HIDE appropriate number of fields based on selection 
+    // END Module 
+
+// END PROGRAM
