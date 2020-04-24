6e424d80e103287542bba6114c8be1bb2c51b4b2
TCashion <58757351+TCashion@users.noreply.github.com>
2020-04-24 09:50:35 -0600

    panel 5 slots 0 and 3 functional

--- public/js/sudoku.js
+++ public/js/sudoku.js
@@ -357,9 +357,65 @@ document.addEventListener("DOMContentLoaded", () => {
                                 };   
                                 break;
                             case 5:
-
+                                if (column === 0) {
+                                    switch (row) {
+                                        case 0:
+                                            unavailable = [gameBoard[p-3][0], gameBoard[p-3][3], gameBoard[p-3][6], gameBoard[p-1][0], gameBoard[p-1][1], gameBoard[p-1][2], gameBoard[p-2][0], gameBoard[p-2][1], gameBoard[p-2][2]];
+                                            thirdTierCounter(0,3,6);
+                                            slotFiller(0);
+                                            break;
+                                        case 1: 
+                                            unavailable = [gameBoard[p-3][0], gameBoard[p-3][3], gameBoard[p-3][6], gameBoard[p-1][0 + 3], gameBoard[p-1][1 + 3], gameBoard[p-1][2 + 3], gameBoard[p-2][0 + 3], gameBoard[p-2][1 + 3], gameBoard[p-2][2 + 3], gameBoard[p][0], gameBoard[p][1], gameBoard[p][2]];
+                                            thirdTierCounter(0,3,6);
+                                            slotFiller(3);
+                                            break;
+                                        // case 2: 
+                                        //     unavailable = [gameBoard[p-3][0], gameBoard[p-3][3], gameBoard[p-3][6], gameBoard[p-1][0 + 6], gameBoard[p-1][1 + 6], gameBoard[p-1][2 + 6], gameBoard[p][0], gameBoard[p][1], gameBoard[p][2], gameBoard[p][3], gameBoard[p][4], gameBoard[p][5]];
+                                        //     thirdTierCounter(0,3,6);
+                                        //     slotFiller(6);
+                                        //     break; 
+                                    }
+                                        
+                                } else if (column === 1) {
+                                    // switch (row) {
+                                    //     case 0: 
+                                    //         unavailable = [gameBoard[p-3][0 + 1], gameBoard[p-3][3 + 1], gameBoard[p-3][6 + 1], gameBoard[p][0], gameBoard[p-1][0], gameBoard[p-1][1], gameBoard[p-1][2]];
+                                    //         thirdTierCounter(1,4,7);
+                                    //         slotFiller(1);
+                                    //         break;
+                                    //     case 1: 
+                                    //         unavailable = [gameBoard[p-3][0 + 1], gameBoard[p-3][3 + 1], gameBoard[p-3][6 + 1], gameBoard[p][0], gameBoard[p-1][0 + 3], gameBoard[p-1][1 + 3], gameBoard[p-1][2 + 3], gameBoard[p][1], gameBoard[p][2], gameBoard[p][3]];
+                                    //         thirdTierCounter(1,4,7);
+                                    //         slotFiller(4);
+                                    //         break;
+                                    //     case 2:
+                                    //         unavailable = [gameBoard[p-3][0 + 1], gameBoard[p-3][3 + 1], gameBoard[p-3][6 + 1], gameBoard[p-1][0 + 6], gameBoard[p-1][1 + 6], gameBoard[p-1][2 + 6], gameBoard[p][0], gameBoard[p][1], gameBoard[p][2], gameBoard[p][3], gameBoard[p][4], gameBoard[p][5], gameBoard[p][6]];
+                                    //         thirdTierCounter(1,4,7);
+                                    //         slotFiller(7);
+                                    //         break;
+                                    // };
+                                } else if (column === 2) {
+                                    // switch (row) {
+                                    //     case 0:
+                                    //         unavailable = [gameBoard[p-3][0 + 2], gameBoard[p-3][3 + 2], gameBoard[p-3][6 + 2], gameBoard[p-1][0], gameBoard[p-1][1], gameBoard[p-1][2], gameBoard[p][0], gameBoard[p][1]];
+                                    //         thirdTierCounter(2,5,8);
+                                    //         slotFiller(2);
+                                    //         break;
+                                    //     case 1: 
+                                    //         unavailable = [gameBoard[p-3][0 + 2], gameBoard[p-3][3 + 2], gameBoard[p-3][6 + 2], gameBoard[p-1][0 + 3], gameBoard[p-1][1 + 3], gameBoard[p-1][2 + 3], gameBoard[p][0], gameBoard[p][1], gameBoard[p][2], gameBoard[p][3], gameBoard[p][4]];
+                                    //         thirdTierCounter(2,5,8);
+                                    //         slotFiller(5);
+                                    //         break;
+                                    //     case 2: 
+                                    //         unavailable = [gameBoard[p-3][0 + 2], gameBoard[p-3][3 + 2], gameBoard[p-3][6 + 2], gameBoard[p-1][0 + 6], gameBoard[p-1][1 + 6], gameBoard[p-1][2 + 6], gameBoard[p][0], gameBoard[p][1], gameBoard[p][2], gameBoard[p][3], gameBoard[p][4], gameBoard[p][5], gameBoard[p][6], gameBoard[p][7]];
+                                    //         thirdTierCounter(2,5,8);
+                                    //         slotFiller(8);
+                                    //         break; 
+                                    // };
+                                };
+                                refreshObj();
                                 break;
-                            case 6: 
+                            case 8: 
 
                                 break; 
                         }
@@ -422,17 +478,17 @@ document.addEventListener("DOMContentLoaded", () => {
                                     secondColumn(2,1);
                                     secondColumn(2,2);
                                     break; 
-                                // case 5: 
-                                //     thirdColumn(0,0);
+                                case 5: 
+                                    thirdColumn(0,0);
                                 //     thirdColumn(0,1);
                                 //     thirdColumn(0,2);
-                                //     thirdColumn(1,0);
+                                    thirdColumn(1,0);
                                 //     thirdColumn(1,1);
                                 //     thirdColumn(1,2);
                                 //     thirdColumn(2,0);
                                 //     thirdColumn(2,1);
                                 //     thirdColumn(2,2);
-                                //     break; 
+                                    break; 
                                 case 6: 
                                     firstColumn(0,0);
                                     firstColumn(0,1);                                
