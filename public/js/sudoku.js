c5b855d63423fcc316d8d6435ca60cb46b382eb5
TCashion <58757351+TCashion@users.noreply.github.com>
2020-04-24 09:55:15 -0600

    panel 5 slots 1 and 4  functional

--- public/js/sudoku.js
+++ public/js/sudoku.js
@@ -376,23 +376,23 @@ document.addEventListener("DOMContentLoaded", () => {
                                             break; 
                                     };
                                 } else if (column === 1) {
-                                    // switch (row) {
-                                    //     case 0: 
-                                    //         unavailable = [gameBoard[p-3][0 + 1], gameBoard[p-3][3 + 1], gameBoard[p-3][6 + 1], gameBoard[p][0], gameBoard[p-1][0], gameBoard[p-1][1], gameBoard[p-1][2]];
-                                    //         thirdTierCounter(1,4,7);
-                                    //         slotFiller(1);
-                                    //         break;
-                                    //     case 1: 
-                                    //         unavailable = [gameBoard[p-3][0 + 1], gameBoard[p-3][3 + 1], gameBoard[p-3][6 + 1], gameBoard[p][0], gameBoard[p-1][0 + 3], gameBoard[p-1][1 + 3], gameBoard[p-1][2 + 3], gameBoard[p][1], gameBoard[p][2], gameBoard[p][3]];
-                                    //         thirdTierCounter(1,4,7);
-                                    //         slotFiller(4);
-                                    //         break;
+                                    switch (row) {
+                                        case 0: 
+                                            unavailable = [gameBoard[p-3][0 + 1], gameBoard[p-3][3 + 1], gameBoard[p-3][6 + 1], gameBoard[p-1][0], gameBoard[p-1][1], gameBoard[p-1][2], gameBoard[p-2][0], gameBoard[p-2][1], gameBoard[p-2][2], gameBoard[p][0]];
+                                            thirdTierCounter(1,4,7);
+                                            slotFiller(1);
+                                            break;
+                                        case 1: 
+                                            unavailable = [gameBoard[p-3][0 + 1], gameBoard[p-3][3 + 1], gameBoard[p-3][6 + 1],  gameBoard[p-1][0 + 3], gameBoard[p-1][1 + 3], gameBoard[p-1][2 + 3], gameBoard[p-2][0 + 3], gameBoard[p-2][1 + 3], gameBoard[p-2][2 + 3], gameBoard[p][0], gameBoard[p][1], gameBoard[p][2], gameBoard[p][3]];
+                                            thirdTierCounter(1,4,7);
+                                            slotFiller(4);
+                                            break;
                                     //     case 2:
                                     //         unavailable = [gameBoard[p-3][0 + 1], gameBoard[p-3][3 + 1], gameBoard[p-3][6 + 1], gameBoard[p-1][0 + 6], gameBoard[p-1][1 + 6], gameBoard[p-1][2 + 6], gameBoard[p][0], gameBoard[p][1], gameBoard[p][2], gameBoard[p][3], gameBoard[p][4], gameBoard[p][5], gameBoard[p][6]];
                                     //         thirdTierCounter(1,4,7);
                                     //         slotFiller(7);
                                     //         break;
-                                    // };
+                                    };
                                 } else if (column === 2) {
                                     // switch (row) {
                                     //     case 0:
@@ -479,10 +479,10 @@ document.addEventListener("DOMContentLoaded", () => {
                                     break; 
                                 case 5: 
                                     thirdColumn(0,0);
-                                //     thirdColumn(0,1);
+                                    thirdColumn(0,1);
                                 //     thirdColumn(0,2);
                                     thirdColumn(1,0);
-                                //     thirdColumn(1,1);
+                                    thirdColumn(1,1);
                                 //     thirdColumn(1,2);
                                     thirdColumn(2,0);
                                 //     thirdColumn(2,1);
