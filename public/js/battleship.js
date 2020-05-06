cf797dae201402f744c0562c15f3fc8a3ac66780
TCashion <58757351+TCashion@users.noreply.github.com>
2020-05-06 17:17:26 -0600

    begin function to render board

--- public/js/battleship.js
+++ public/js/battleship.js
@@ -1,35 +1,50 @@
 /*----- constants -----*/
 
-    const playerColors = {
-        "1": "var(--main-minus-one)",         // player1
-        "-1": "var(--main-color)"             // player2
-    };
-    const hitMissColors = {
-        "null": "var(--main-plus-one)",
-        "miss": "var(--main-plus-two)",
-        "hit": "orange",
-        "ship": "var(--main-minus-two)"
-    };
-    boardLength = 4;
+const battleshipPlayerColors = {
+    "1": "var(--main-minus-one)",         // player1
+    "-1": "var(--main-color)"             // player2
+};
+const hitMissColors = {
+    "null": "var(--main-plus-one)",
+    "miss": "var(--main-plus-two)",
+    "hit": "orange",
+    "ship": "var(--main-minus-two)"
+};
+boardLength = 4;
 
 /*----- app's state (variables) -----*/
 
-    let turn;
-    let winner;
-    let playerOneShips;
-    let playerTwoShips;
-    let playerOneBoard;
-    let playerOneRadar;
-    let playerTwoBoard;
-    let playerTwoRadar;
-    let engageAi;                 
-    let aiHits;
-    let shipIdentified;
+let bsTurn;
+let bsWinner;
+let playerOneShips;
+let playerTwoShips;
+let playerTwoDisplay;
+let playerTwoRadar;
+let engageAi;                 
+let aiHits;
+let shipIdentified;
 
 /*----- cached element references -----*/
 
+const playerOneRadar = document.getElementById("playerOneRadar");
+const playerOneDisplay = document.getElementById("playerOneDisplay");
+const targetDisplay = document.getElementsByClassName("battleship-target-display");
 
 /*----- event listeners -----*/
 
 
 /*----- functions -----*/
+
+// create gameboard on page load
+function createBoard(boardLength) {
+    for (let i = 0; i <= boardLength; i++) {
+        const div = document.createElement("div");
+        div.style.height = "20px";
+        div.style.width = "20px";
+        div.style.backgroundColor = "black";
+        playerOneRadar.append(div);
+    }
+}
+createBoard(boardLength);
+
+// make a grid layout 
